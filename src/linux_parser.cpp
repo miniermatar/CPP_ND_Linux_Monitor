1dc9fbe19a017c4e4e2833d1bd1af7dfb0734061
miniermatar <minier.matar@gmail.com>
2021-05-15 19:28:55 +0300

    first commit

---
+++ src/linux_parser.cpp
@@ -0,0 +1,189 @@
+#include <dirent.h>
+#include <unistd.h>
+#include <string>
+#include <vector>
+
+#include "linux_parser.h"
+
+using std::stof;
+using std::string;
+using std::to_string;
+using std::vector;
+
+std::string parameter(std::string filepath, std::string key_name) {
+    string line;
+    string key,key_value;
+    string value;
+    std::ifstream filestream(filepath);
+    if (filestream.is_open()) {
+      while (std::getline(filestream, line)) {
+        std::istringstream linestream(line);
+        linestream >> key >> value;
+          if (strncmp(key.c_str(),key_name.c_str(),key_name.length())==0) {
+            key_value=value;
+            break;
+          }
+        }
+      }
+    return key_value;
+}
+
+
+string LinuxParser::OperatingSystem() {
+  string line;
+  string key;
+  string value;
+  std::ifstream filestream(kOSPath);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::replace(line.begin(), line.end(), ' ', '_');
+      std::replace(line.begin(), line.end(), '=', ' ');
+      std::replace(line.begin(), line.end(), '"', ' ');
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        if (key == "PRETTY_NAME") {
+          std::replace(value.begin(), value.end(), '_', ' ');
+          return value;
+        }
+      }
+    }
+  }
+  return value;
+}
+
+string LinuxParser::Kernel() {
+  string os, version, kernel;
+  string line;
+  std::ifstream stream(kProcDirectory + kVersionFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> os >> version >> kernel;
+  }
+  return kernel;
+}
+
+// BONUS: Update this to use std::filesystem
+vector<int> LinuxParser::Pids() {
+  vector<int> pids;
+  DIR* directory = opendir(kProcDirectory.c_str());
+  struct dirent* file;
+  while ((file = readdir(directory)) != nullptr) {
+    // Is this a directory?
+    if (file->d_type == DT_DIR) {
+      // Is every character of the name a digit?
+      string filename(file->d_name);
+      if (std::all_of(filename.begin(), filename.end(), isdigit)) {
+        int pid = stoi(filename);
+        pids.push_back(pid);
+      }
+    }
+  }
+  closedir(directory);
+  return pids;
+}
+
+// TODO: Read and return the system memory utilization
+float LinuxParser::MemoryUtilization() {
+    float MemTotal, MemFree;
+    string line;
+    string key;
+    string value;
+    std::ifstream filestream(kProcDirectory + kMeminfoFilename);
+    if (filestream.is_open()) {
+      while (std::getline(filestream, line)) {
+        line.erase(std::remove(line.begin(), line.end(), ' '),line.end());
+        std::replace(line.begin(), line.end(), ':', ' ');
+        line.erase(std::remove(line.begin(), line.end(), 'k'),line.end());
+        line.erase(std::remove(line.begin(), line.end(), 'B'),line.end());
+        std::istringstream linestream(line);
+        linestream >> key >> value;
+          if (key == "MemTotal") {
+            MemTotal= std::stof(value);
+          } else if (key == "MemFree") {
+              MemFree= std::stof(value);
+              break;
+          }
+        }
+      }
+    return ((MemTotal-MemFree)/(MemTotal));
+}
+
+// TODO: Read and return the system uptime
+long LinuxParser::UpTime() {
+
+    long uptime;
+    string line, uptime_string;
+    std::ifstream stream(kProcDirectory + kUptimeFilename);
+    if (stream.is_open()) {
+      std::getline(stream, line);
+      std::istringstream linestream(line);
+      linestream >> uptime_string;
+      uptime = std::stof(uptime_string);
+    }
+
+    return uptime; }
+
+// TODO: Read and return the number of jiffies for the system
+long LinuxParser::Jiffies() { return 0; }
+
+// TODO: Read and return the number of active jiffies for a PID
+// REMOVE: [[maybe_unused]] once you define the function
+long LinuxParser::ActiveJiffies(int pid[[maybe_unused]]) { return 0; }
+
+// TODO: Read and return the number of active jiffies for the system
+long LinuxParser::ActiveJiffies() { return 0; }
+
+// TODO: Read and return the number of idle jiffies for the system
+long LinuxParser::IdleJiffies() { return 0; }
+
+// TODO: Read and return CPU utilization
+vector<string> LinuxParser::CpuUtilization() { return {}; }
+
+// TODO: Read and return the total number of processes
+int LinuxParser::TotalProcesses() {
+    std::string value = parameter(kProcDirectory + kStatFilename,"processes");
+    return std::stoi(value);
+
+    /*
+    int t_processes;
+    string line;
+    string key;
+    string value;
+    std::ifstream filestream(kProcDirectory + kStatFilename);
+    if (filestream.is_open()) {
+      while (std::getline(filestream, line)) {
+        std::istringstream linestream(line);
+        linestream >> key >> value;
+          if (key == "processes") {
+            t_processes= std::stoi(value);
+            break;
+          }
+        }
+      }
+    return t_processes;
+*/
+}
+
+// TODO: Read and return the number of running processes
+int LinuxParser::RunningProcesses() { return 0; }
+
+// TODO: Read and return the command associated with a process
+// REMOVE: [[maybe_unused]] once you define the function
+string LinuxParser::Command(int pid[[maybe_unused]]) { return string(); }
+
+// TODO: Read and return the memory used by a process
+// REMOVE: [[maybe_unused]] once you define the function
+string LinuxParser::Ram(int pid[[maybe_unused]]) { return string(); }
+
+// TODO: Read and return the user ID associated with a process
+// REMOVE: [[maybe_unused]] once you define the function
+string LinuxParser::Uid(int pid[[maybe_unused]]) { return string(); }
+
+// TODO: Read and return the user associated with a process
+// REMOVE: [[maybe_unused]] once you define the function
+string LinuxParser::User(int pid[[maybe_unused]]) { return string(); }
+
+// TODO: Read and return the uptime of a process
+// REMOVE: [[maybe_unused]] once you define the function
+long LinuxParser::UpTime(int pid[[maybe_unused]]) { return 0; }
